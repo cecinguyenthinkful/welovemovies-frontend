5f353100d21b7d4ab45276650faa535734dff8c6
Cecile Nguyen <ctnguyen2015@gmail.com>
2024-02-11 14:22:37 -0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
